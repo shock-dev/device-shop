1574dccfe32805a28fb4228e4317bfdc7f12fb20
Andrey <shock.work@mail.ru>
2021-01-15 14:23:56 +0300

    template for logo and search part

--- src/components/app/dsHeader.vue
+++ src/components/app/dsHeader.vue
@@ -1,13 +1,35 @@
 <template>
   <header class="header">
     <div class="container">
-      <div class="header__inner"></div>
+      <div class="header__inner">
+        <router-link class="header__logo" :to="{ name: 'Home' }">
+          <img src="~@/assets/img/logo.png" alt="Logo">
+        </router-link>
+        <div class="header__row">
+          <div class="header__col search flex" :class="{ active: searchActive }">
+            <label class="search__label">
+              <input
+                class="search__input"
+                type="text"
+                placeholder="Поиск по сайту"
+                @focus="searchActive = true"
+                @focusout="searchActive = false"
+              >
+            </label>
+            <button class="search__btn">Найти</button>
+          </div>
+          <div class="header__col"></div>
+        </div>
+      </div>
     </div>
   </header>
 </template>
 
 <script>
 export default {
-  name: "dsHeader"
+  name: "dsHeader",
+  data: () => ({
+    searchActive: false
+  })
 }
 </script>
