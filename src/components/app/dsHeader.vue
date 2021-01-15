d9c3d8e6cf39c4864a3dd18bf056e44c015a8d1e
Andrey <shock.work@mail.ru>
2021-01-15 12:10:35 +0300

    new header component

---
+++ src/components/app/dsHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__inner"></div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "dsHeader"
+}
+</script>
