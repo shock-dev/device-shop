558377f428da73d4d16103bdfd0a30170b8edbbf
Andrey <shock.work@mail.ru>
2021-01-31 20:07:26 +0300

    new component for breadcrumbs + template

---
+++ src/components/app/dsBreadcrumbs.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="breadcrumbs">
+    <ul class="breadcrumbs__list flex">
+      <li class="breadcrumbs__item">
+        <router-link class="breadcrumbs__link" to="">Главная</router-link>
+      </li>
+      <li class="breadcrumbs__item">
+        <router-link class="breadcrumbs__link" to="">Каталог товаров</router-link>
+      </li>
+      <li class="breadcrumbs__item">
+        <router-link class="breadcrumbs__link" to="">Моноподы для селфи</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dsBreadcrumbs"
+}
+</script>
