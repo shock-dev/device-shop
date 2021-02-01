80a39f8fdd8970c1780e2caaa1bd50869241f898
Andrey <shock.work@mail.ru>
2021-02-01 20:12:32 +0300

    started template for service

--- src/components/home/dsServices.vue
+++ src/components/home/dsServices.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="services"></div>
+  <div class="services">
+    <div class="container">
+      <div class="services__inner">
+        <div class="services__tabs-box">
+          <button class="services__tab active">Доставка</button>
+          <button class="services__tab">Гарантия</button>
+          <button class="services__tab">Кредит</button>
+        </div>
+        <div class="services__content">
+          <div class="services__content-item">
+            <div class="services__content-info">
+              <div class="services__content-title">Доставка</div>
+              <p class="services__content-text">
+                Мы с удовольствием доставим ваш товар прямо к вашему подъезду совершенно бесплатно! Ведь мы неплохо заработаем, поднимая его на ваш этаж.
+              </p>
+            </div>
+            <img src="~@/assets/img/delivery-image.png" alt="Delivery">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
