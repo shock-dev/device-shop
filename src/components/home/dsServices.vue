8eb1b0cd005dda6dc2ff626b7e1b5555be76287b
Andrey <shock.work@mail.ru>
2021-02-01 20:39:19 +0300

    new data for content + realized logic tabs

--- src/components/home/dsServices.vue
+++ src/components/home/dsServices.vue
@@ -12,14 +12,19 @@
           >{{ tab.title }}</button>
         </div>
         <div class="services__content">
-          <div class="services__content-item">
+          <div
+            class="services__content-item"
+            v-for="item in content"
+            :key="item.id"
+            :class="{ hide: activeTab !== item.id }"
+          >
             <div class="services__content-info">
-              <div class="services__content-title">Доставка</div>
+              <div class="services__content-title">{{ item.title }}</div>
               <p class="services__content-text">
-                Мы с удовольствием доставим ваш товар прямо к вашему подъезду совершенно бесплатно! Ведь мы неплохо заработаем, поднимая его на ваш этаж.
+                {{ item.text }}
               </p>
             </div>
-            <img src="~@/assets/img/delivery-image.png" alt="Delivery">
+            <img :src="item.imageUrl" :alt="item.title">
           </div>
         </div>
       </div>
@@ -45,7 +50,27 @@ export default {
         title: 'Кредит'
       }
     ],
-    activeTab: 0
+    activeTab: 1,
+    content: [
+      {
+        id: 1,
+        title: 'Доставка',
+        text: 'Мы с удовольствием доставим ваш товар прямо к вашему подъезду совершенно бесплатно! Ведь мы неплохо заработаем, поднимая его на ваш этаж.',
+        imageUrl: require('@/assets/img/delivery-image.png')
+      },
+      {
+        id: 2,
+        title: 'Гарантия',
+        text: 'Мы с удовольствием доставим ваш товар прямо к вашему подъезду совершенно бесплатно! Ведь мы неплохо заработаем, поднимая его на ваш этаж.',
+        imageUrl: require('@/assets/img/delivery-image.png')
+      },
+      {
+        id: 3,
+        title: 'Кредит',
+        text: 'Мы с удовольствием доставим ваш товар прямо к вашему подъезду совершенно бесплатно! Ведь мы неплохо заработаем, поднимая его на ваш этаж.',
+        imageUrl: require('@/assets/img/delivery-image.png')
+      }
+    ]
   })
 }
 </script>
