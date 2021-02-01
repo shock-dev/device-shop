ddf818fe444b80ef6ffe7501ab2b2f813368a059
Andrey <shock.work@mail.ru>
2021-02-01 18:22:18 +0300

    new component for categories

---
+++ src/components/home/dsCategories.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="categories"></div>
+</template>
+
+<script>
+export default {
+  name: "dsCategories"
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
     <ds-slider/>
+    <ds-categories/>
   </div>
 </template>
 
 <script>
 import DsSlider from "@/components/home/dsSlider"
+import DsCategories from "@/components/home/dsCategories"
 
 export default {
   name: "Home",
   components: {
+    DsCategories,
     DsSlider
   }
 }
