ecb8e44cb4eef22da7fed36bc6e625eb6765f62d
Andrey <shock.work@mail.ru>
2021-02-01 13:10:34 +0300

    integrate hooper in slider component

--- src/components/home/dsSlider.vue
+++ src/components/home/dsSlider.vue
@@ -1,9 +1,34 @@
 <template>
-  <div class="home__slider"></div>
+  <div class="home__slider">
+    <div class="container">
+      <hooper>
+        <slide>
+          slide 1
+        </slide>
+        <slide>
+          slide 2
+        </slide>
+
+        <hooper-pagination slot="hooper-addons"></hooper-pagination>
+      </hooper>
+      </div>
+    </div>
 </template>
 
 <script>
+import {
+  Hooper,
+  Slide,
+  Pagination as HooperPagination
+} from 'hooper'
+import 'hooper/dist/hooper.css'
+
 export default {
-  name: "dsSlider"
+  name: "dsSlider",
+  components: {
+    Hooper,
+    Slide,
+    HooperPagination
+  }
 }
 </script>
