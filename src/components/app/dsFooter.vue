e68252aa17af3df7f876163c084f0ec3b22fc278
Andrey <shock.work@mail.ru>
2021-02-02 13:57:53 +0300

    files for new component - footer

---
+++ src/components/app/dsFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="footer__inner"></div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "dsFooter"
+}
+</script>
