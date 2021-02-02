ffdfeddb315096ef941407545a9e75a61dad19ba
Andrey <shock.work@mail.ru>
2021-02-02 15:30:20 +0300

    template for top part of footer

--- src/components/app/dsFooter.vue
+++ src/components/app/dsFooter.vue
@@ -1,7 +1,33 @@
 <template>
   <footer class="footer">
     <div class="container">
-      <div class="footer__inner"></div>
+      <div class="footer__inner">
+        <div class="footer__head">
+          <router-link class="footer__logo" to="">
+            <img src="~@/assets/img/footer-logo.png" alt="Logo">
+          </router-link>
+          <div class="footer__actions flex">
+            <button class="footer__action">
+              <svg class="footer__action-svg" width="13" height="12">
+                <use href="~@/assets/img/svg/sprite.svg#login"></use>
+              </svg>
+              <span class="footer__action-text">Войти</span>
+            </button>
+            <button class="footer__action">
+              <svg class="footer__action-svg" width="10" height="10">
+                <use href="~@/assets/img/svg/sprite.svg#compare"></use>
+              </svg>
+              <span class="footer__action-text">Сравнить</span>
+            </button>
+            <button class="footer__action">
+              <svg class="footer__action-svg" width="12" height="12">
+                <use href="~@/assets/img/svg/sprite.svg#cart"></use>
+              </svg>
+              <span class="footer__action-text">Корзина</span>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
