1f0ee99d317ee9931809bb9965de942c4aab4d5f
Andrey <shock.work@mail.ru>
2021-02-02 13:38:48 +0300

    template for contacts component

--- src/components/home/dsContacts.vue
+++ src/components/home/dsContacts.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="contacts"></div>
+  <div class="contacts">
+    <div class="feedback__title">Контакты</div>
+    <p class="feedback__text">Вы можете забрать товар сами, заехав в наш офис. Заодно, вы сможете проверить работоспособность покупки. Всякое бывает.</p>
+    <iframe class="feedback__map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.1109162690773!2d37.529607840945886!3d55.687059365408636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54cf65f5c8955%3A0x694710ccd55501e!2z0YPQuy4g0KHRgtGA0L7QuNGC0LXQu9C10LksIDE1LCDQnNC-0YHQutCy0LAsIDExOTMxMQ!5e0!3m2!1sru!2sru!4v1612261785262!5m2!1sru!2sru" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    <button class="feedback__btn">Напишите нам</button>
+  </div>
 </template>
 
 <script>
