9725f690a9f79282e9638cc3b79cbece37ca72da
Andrey <shock.work@mail.ru>
2021-02-02 13:15:56 +0300

    template for about component and integrate it to feedback component

---
+++ src/components/home/dsAbout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <div class="feedback__title">О нас</div>
+    <p class="feedback__text">Огромный выбор гаджетов не оставит равнодушным гика, который есть в каждом из нас.</p>
+    <p class="feedback__text">Мы можем доставить ваш товар в самые отдаленные точки России! DEVICE работает со многими транспортными компаниями:</p>
+    <ul class="feedback__list">
+      <li class="feedback__list-item">Деловые Линии</li>
+      <li class="feedback__list-item">Автотрейдинг</li>
+      <li class="feedback__list-item">ЖелДорЭкспедиция</li>
+    </ul>
+    <button class="feedback__btn">Подробнее о нас</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dsAbout"
+}
+</script>
--- src/components/home/dsFeedback.vue
+++ src/components/home/dsFeedback.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="row"></div>
+  <div class="container">
+    <div class="row feedback">
+      <div class="col-50 feedback__item">
+        <ds-about/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import DsAbout from "@/components/home/dsAbout"
+
 export default {
-  name: "dsFeedback"
+  name: "dsFeedback",
+  components: {
+    DsAbout
+  }
 }
 </script>
